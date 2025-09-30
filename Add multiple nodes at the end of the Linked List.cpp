a1382c002275937940223c2d0f0e208de7ad0bb3
Ratnesh Kumar <ratneshkr129@gmail.com>
2025-09-30 19:41:04 +0530

    Update and rename 02.cpp to Add multiple nodes at the end of the Linked List.cpp

---
+++ Add multiple nodes at the end of the Linked List.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int data) {
+        val = data;
+        next = NULL;
+    }
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = NULL;
+    }
+
+    void insertAtEnd(int data) {
+        Node* newNode = new Node(data);
+        if (head == NULL) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->val << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtEnd(30);
+    list.insertAtEnd(40);
+
+    cout << "Linked List: ";
+    list.display();
+
+    return 0;
+}
+
+OUTPUT :
+Linked List: 10 20 30 40 
